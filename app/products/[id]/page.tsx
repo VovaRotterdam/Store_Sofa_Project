3892b4a6744e593fdd1bcc2d27bfed225d9b5336
VovaRotterdam <volodimirshypka@gmail.com>
2025-07-10 17:00:22 +0200

    fixed props in products/id/page.tsx

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -7,7 +7,13 @@ import FavoriteToggleButton from "@/components/products/FavoriteToggleButton";
 import AddToCart from "@/components/single-product/AddToCart";
 import ProductRating from "@/components/single-product/ProductRating";
 
-async function SingleProductPage({ params }: { params: { id: string } }) {
+type Props = {
+    params: {
+        id: string;
+    };
+};
+
+async function SingleProductPage({ params }: Props) {
     const product = await fetchSingleProduct(params.id);
 
     const { name, image, price, description, company } = product;
